457a5a32a646429b6a5c9c09cfe1eec2e2f8e29c
Vaibhav Sharma <sharmavaibhav5796@gmail.com>
2021-01-07 02:38:16 +0530

    .

---
+++ src/components/Loader.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="container mt-5 d-flex justify-content-center align-items-center">
+      <div
+        id="loader"
+        className="d-flex justify-content-center align-items-center p-5"
+      >
+        Please wait while loading...
+      </div>
+    </div>
+  );
+};
+export default Loader;
